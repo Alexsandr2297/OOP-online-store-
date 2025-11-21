73d897cae7489043018163413ed6422e171b8ef5
Alexsandr Varenyshev <warrn11@mail.ru>
2025-11-21 12:33:06 +0300

    добавление_проверочного_кода_main

--- src/category.py
+++ src/category.py
@@ -5,18 +5,18 @@ class Category:
     products: list  # список товаров продуктов
 
     # Переменная на уровне класса для подсчета количества категорий и товаров
-    count_category = 0  # количество категорий
-    count_product = 0  # количество товаров
+    category_count = 0  # количество категорий
+    product_count = 0  # количество товаров
 
     def __init__(self, name, description, products):
         """Метод, который инициализирует экземпляры класса."""
         self.name = name
         self.description = description
         self.products = products
-        Category.count_category += 1
-        Category.count_product += len(self.products)
+        Category.category_count += 1
+        Category.product_count += len(self.products)
 
     def add_product(self, product):
         """Добавляет продукт в категорию и увеличивает счетчик"""
-        Category.count_product += 1
+        Category.product_count += 1
         self.products.append(product)
--- tests/test_category.py
+++ tests/test_category.py
@@ -6,29 +6,30 @@ from src.product import Product
 
 @pytest.fixture()
 def category():
-    products = [Product('Samsung Galaxy C25 Ultra',
-                        'Смартфоны, как средство не только коммуникации, но и получение дополнительных '
-                        'функций для удобства жизни', 54980, 5),
-                Product('Iphone 16', '512GB, Gray space', 49.000, 5),
-                Product('Xiaomi Redmi Note 15', '1024GB, Синий', 48.000, 5)]
-    return Category('Смартфоны', '256GB, Серый цвет, 200MP камера', products)
+    products = [Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+                Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+                Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)]
+    return Category("Смартфоны",
+                    ('Смартфоны, как средство не только коммуникации, '
+                     'но и получения дополнительных функций для удобства жизни'), products)
 
 
 def test_init(category):
     assert category.name == 'Смартфоны'
-    assert category.description == '256GB, Серый цвет, 200MP камера'
+    assert category.description == ('Смартфоны, как средство не только коммуникации, '
+                                    'но и получения дополнительных функций для удобства жизни')
     assert len(category.products) == 3
-    assert category.count_category == 1
-    assert category.count_product == 3
+    assert category.category_count == 1
+    assert category.product_count == 3
 
 
 def test_add_product(category):
     count = len(category.products)  # = 3
-    total = Category.count_product  # = 3
+    total = Category.product_count  # = 3
 
     new_product = Product('Honor', 'Смартфоны, как средство не только коммуникации, но и получение дополнительных '
-                        'функций для удобства жизни', 26699, 1)
+                                   'функций для удобства жизни', 26699, 1)
     category.add_product(new_product)
     assert len(category.products) == count + 1  # = 4
     assert category.products[-1] == new_product
-    assert Category.count_product == total + 1  # = 4
+    assert Category.product_count == total + 1  # = 4
