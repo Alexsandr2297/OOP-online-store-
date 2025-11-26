e50d79768b91e5b4da481ce8e5a48a99b84dbb1a
Alexsandr Varenyshev <warrn11@mail.ru>
2025-11-26 12:21:11 +0300

    dz_№_14.2

--- src/category.py
+++ src/category.py
@@ -12,11 +12,22 @@ class Category:
         """Метод, который инициализирует экземпляры класса."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
 
     def add_product(self, product):
         """Добавляет продукт в категорию и увеличивает счетчик"""
         Category.product_count += 1
-        self.products.append(product)
+        self.__products.append(product)
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. (Остаток: {product.quantity} шт.)\n"
+        return products_str
+
+    @property
+    def list_prod(self):
+        return self.__products
